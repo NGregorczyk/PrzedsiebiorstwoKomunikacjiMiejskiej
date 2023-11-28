267d50287982382df67ec8ba80d91b4655260c8a
NGregorczyk <natalia.gregorczyk002@gmail.com>
2023-11-28 15:02:29 +0100

    Linie

--- src/pl/gornik/Main.java
+++ src/pl/gornik/Main.java
@@ -1,24 +1,101 @@
 package pl.gornik;
 
+import pl.gornik.przystanek.Przystanek;
+
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<Przystanek> Linia1 = new ArrayList<Przystanek>();
+        Linia1.add(new Przystanek("9:20", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("9:35", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("9:40", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:00", "ul.Partyzantów 15b", 1));
 
+        Linia1.add(new Przystanek("10:15", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("10:30", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("10:35", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:55", "ul.Partyzantów 15b", 1));
+
+        Linia1.add(new Przystanek("11:10", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("11:25", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("11:30", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("11:50", "ul.Partyzantów 15b", 1));
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<Przystanek> Linia2 = new ArrayList<Przystanek>();
+        Linia1.add(new Przystanek("9:20", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("9:35", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("9:40", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:00", "ul.Partyzantów 15b", 1));
+
+        Linia1.add(new Przystanek("10:15", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("10:30", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("10:35", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:55", "ul.Partyzantów 15b", 1));
+
+        Linia1.add(new Przystanek("11:10", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("11:25", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("11:30", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("11:50", "ul.Partyzantów 15b", 1));
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<Przystanek> Linia3 = new ArrayList<Przystanek>();
+        Linia1.add(new Przystanek("9:20", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("9:35", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("9:40", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:00", "ul.Partyzantów 15b", 1));
+
+        Linia1.add(new Przystanek("10:15", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("10:30", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("10:35", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:55", "ul.Partyzantów 15b", 1));
+
+        Linia1.add(new Przystanek("11:10", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("11:25", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("11:30", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("11:50", "ul.Partyzantów 15b", 1));
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ArrayList<Przystanek> Linia4 = new ArrayList<Przystanek>();
+        Linia1.add(new Przystanek("9:20", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("9:35", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("9:40", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:00", "ul.Partyzantów 15b", 1));
+
+        Linia1.add(new Przystanek("10:15", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("10:30", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("10:35", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("10:55", "ul.Partyzantów 15b", 1));
+
+        Linia1.add(new Przystanek("11:10", "ul.Zachodnia 20", 1));
+        Linia1.add(new Przystanek("11:25", "ul.Szkolna 10a", 1));
+        Linia1.add(new Przystanek("11:30", "ul.Szkolna 3", 1));
+        Linia1.add(new Przystanek("11:50", "ul.Partyzantów 15b", 1));
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         String password = "xyz";
         boolean hasloWprowadzone = false;
 
         while (true) {
             System.out.println("1.Pasażer");
-            System.out.println("2.pl.gornik.Pracownik.pl.gornik.Pracownik");
+            System.out.println("2.Pracownik");
             int choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
                 case 1:
-                    pasażer(scanner);
+                    pasazer(scanner);
                     break;
 
                 case 2:
@@ -42,7 +119,7 @@ public class Main {
         }
     }
 
-    private static void pasażer(Scanner scanner) {
+    private static void pasazer(Scanner scanner) {
         System.out.println("1.Rozkład jazdy autobusów");
         System.out.println("2.Rozkład jazdy tramwajów");
         System.out.println("3.Linie tramwajów przejeżdające przez stacje");
@@ -56,19 +133,23 @@ public class Main {
         switch (choice) {
             case 1:
                 System.out.println("Rozkład jazdy autobusów");
-                //wybor linii przez
+                //wybor linii przez scanner
                 break;
             case 2:
                 System.out.println("Rozkład jazdy tramwajów");
+                //wybor linii przez scanner
                 break;
             case 3:
                 System.out.println("Linie tramwajów przejeżdające przez stacje");
+                //linie przejezdzajace przez stacje, przyrownanie scannera do nazwy stacji
                 break;
             case 4:
                 System.out.println("Linie autobusów przejeżdżające przez przystanki");
+                //linie przejezdzajace przez stacje, przyrownanie scannera do nazwy przystanka
                 break;
             case 5:
                 System.out.println("Koszt biletów");
+                //metoda do obliczania kosztu biletow, ulgowe i zwykle w zaleznosci od ilosci przystankow
                 break;
             case 6:
                 System.out.println("Wyjście");
@@ -99,6 +180,7 @@ public class Main {
                     break;
                 case 3:
                     System.out.println("Wynagrodzenie");
+                    //metoda do obliczania wynagrodzenia
                     break;
                 case 4:
                     System.out.println("Wyjście");
@@ -125,15 +207,19 @@ public class Main {
             switch (choice) {
                 case 1:
                     System.out.println("Zwolnienie pracowników");
+                    //metoda do zwalniania pracownikow
                     break;
                 case 2:
                     System.out.println("Zatrudnienie pracowników");
+                    //metoda do zatrudniania pracownikow
                     break;
                 case 3:
                     System.out.println("Przydzielenie motorniczych do tramwajów");
+                    //metoda do przydzielania pracownikow do tramwajow, sprawdzajaca czy ma tramwaj ma linie
                     break;
                 case 4:
                     System.out.println("Przydzielenie kierowców do autobusów");
+                    //metoda do przydzielania pracownikow do autobusow, sprawdzajaca czy ma autobus ma linie
                     break;
                 case 5:
                     System.out.println("Powrót");
@@ -149,35 +235,51 @@ public class Main {
     private static void zarządzaniePojazdem(Scanner scanner) {
         int choice;
         do {
-            System.out.println("1.Zwolnienie pracowników");
-            System.out.println("2.Zatrudnienie pracowników");
-            System.out.println("3.Przydzielenie motorniczych do tramwajów");
-            System.out.println("4.Przydzielenie kierowców do autobusów");
-            System.out.println("5.Powrót");
+            System.out.println("1.Dodawanie tramwajów");
+            System.out.println("2.Usuwanie tramwajów");
+            System.out.println("3.Dodawanie autobusów");
+            System.out.println("4.Usuwanie autobusów");
+            System.out.println("5.Przydzielenie tramwajow do linii");
+            System.out.println("6.Przydzielenie autobusow do linii");
+            System.out.println("7.Powrót");
 
             choice = scanner.nextInt();
             scanner.nextLine();
 
             switch (choice) {
                 case 1:
-                    System.out.println("Zwolnienie pracowników");
+                    System.out.println("1.Dodawanie tramwajów");
+                    //metoda do dodawania tramwajow, przypisywaniu do tramwaju pracownika, jesli nie ma pracownika to tramwaj bez linii i pracownika
+                    //jesli dodany pracownik dodanie go do liniii.
                     break;
                 case 2:
-                    System.out.println("Zatrudnienie pracowników");
+                    System.out.println("2.Usuwanie tramwajów");
+                    //metoda do usuwania tramwajow
                     break;
                 case 3:
-                    System.out.println("Przydzielenie motorniczych do tramwajów");
+                    System.out.println("3.Dodawanie autobusów");
+                    //metoda do dodawania autobusow, przypisywaniu do autobusu pracownika, jesli nie ma pracownika to autobus bez linii i pracownika
+                    //jesli dodany pracownik dodanie go do liniii.
                     break;
                 case 4:
-                    System.out.println("Przydzielenie kierowców do autobusów");
+                    System.out.println("4.Usuwanie autobusów");
+                    // //metoda do usuwania tramwajow.
                     break;
                 case 5:
+                    System.out.println("5.Przydzielenie tramwajow do linii");
+                    //metoda dodawanie tramwajow do linii, gdy tramwaj nie ma kierowcy trzeba najpierw przypisac motorniczego
+                    break;
+                case 6:
+                    System.out.println("6.Przydzielenie autobusow do linii");
+                    //metoda dodawanie autobusow do linii, gdy autobus nie ma kierowcy trzeba najpierw przypisac kierowce
+                    break;
+                case 7:
                     System.out.println("Powrót");
                     break;
                 default:
                     System.out.println("Nieprawidłowy wybór");
                     break;
             }
-        } while (choice != 5);
+        } while (choice != 7);
     }
 }
--- src/pl/gornik/przystanek/Przystanek.java
+++ src/pl/gornik/przystanek/Przystanek.java
@@ -3,7 +3,8 @@ package pl.gornik.przystanek;
 public class Przystanek {
     String godzinaPrzyjazdu;
 
-
+private static int id=1;
+int temp;
     String nazwaUlicy;
 
     int linia;
@@ -12,16 +13,17 @@ public class Przystanek {
         this.godzinaPrzyjazdu = godzinaPrzyjazdu;
         this.nazwaUlicy = nazwaUlicy;
         this.linia = linia;
-
+        this.temp=id++;
 
     }
-
+//dorobic gettery i settery i dolozyc do tostringa i do obiektow id i zrobic enkapsulacje//////////////////////////////////////////////////////////
     public Przystanek() {
     }
 
     @Override
     public String toString() {
         return "pl.gornik.Przystanek.pl.gornik.Przystanek{" +
+
                 "godzinaPrzyjazdu='" + godzinaPrzyjazdu + '\'' +
                 ", nazwaUlicy='" + nazwaUlicy + '\'' +
                 '}';
